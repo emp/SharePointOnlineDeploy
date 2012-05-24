6c7e571d006f234e501597ef7fd858ae31ce66d8
Francisco Aquino <thewelk@gmail.com>
2012-05-24 15:08:33 -0300

    'frist'

---
+++ README.md
@@ -0,0 +1,53 @@
+
+#Warning
+
+**This is a quick hack** and the same functionality is implemented in Visual Studio 11 -- if you are stuck with Visual Studio 2010 and want some sandboxed solution deployment automation, this might work for you.
+
+p.s.: if you have a better way to [de]activate a sandbox solution using the Client OM, please let me know!
+
+#Steps
+
+###1. Modify the _catalogs/solutions/Forms/Activate.aspx page in your 365 site.
+
+Place following the script inside the PlaceHolderMain ContentPlaceHolder, before the `</asp:Content>` fragment:
+
+    <script>
+
+	    if(location.href.indexOf("deploy=true") > -1) {
+
+		    ExecuteOrDelayUntilScriptLoaded(deploySolution, "SP.Ribbon.js");
+	    }
+	
+	    function deploySolution() {
+
+		    if(location.href.indexOf("Op=ACT") > -1)
+		    {
+			    SP.Ribbon.PageManager.get_instance().$k_1[0].handleCommand("Ribbon.ListForm.Display.Solution.ActivateSolution");
+		    }
+		
+		    if(location.href.indexOf("Op=DEA") > -1)
+		    {
+			    SP.Ribbon.PageManager.get_instance().$k_1[0].handleCommand("Ribbon.ListForm.Display.Solution.DeactivateSolution");
+		    }
+	
+	    }
+	
+    </script>
+
+###2. Add the following on your Pre or Post Solution Deployment step in the SharePoint tab (Visual Studio project):**
+
+"C:/path/to/SharePointOnlineDeploy.exe" "https://365site/" "$(TargetDir)$(TargetName).wsp"
+
+#Thanks
+
+- Claims Authentication based on the official Microsoft sample http://msdn.microsoft.com/en-us/library/hh147177.aspx, modified to support proxies.
+- PeppeDotNet http://solutioninstaller365.codeplex.com for the Site.GetCatalog(121) trick and basic Client OM file upload code;
+
+#The MIT License (MIT)
+Copyright (c) 2012 Francisco Aquino *faquino@live.com*
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ SharePointOnlineDeploy/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.SharePoint.Client;
+using MSDN.Samples.ClaimsAuth;
+
+namespace SharePointOnlineDeploy
+{
+  class Program
+  {
+    private static System.Windows.Forms.Form _deactivation;
+    private static System.Windows.Forms.Form _activation;
+    private static string _path;
+    private static string _site;
+    private static ClientContext _context;
+    private static bool _isDeactivated;
+
+    [STAThread]
+    static void Main(string[] args)
+    {
+
+      try
+      {
+
+        if (args.Length < 2)
+          throw new Exception("Expected atleast 2 parameters: deployWSP.exe <url> <wsp_path> <debug>");
+
+        _site = args[0];
+        _path = args[1];
+
+        using (_context = ClaimClientContext.GetAuthenticatedContext(_site))
+        {
+
+          if (_context != null)
+          {
+            _context.Load(_context.Site);
+            _context.ExecuteQuery();
+
+            var wspPath = _context.Site.ServerRelativeUrl + "_catalogs/solutions/" + System.IO.Path.GetFileName(_path);
+            var wsp = _context.Web.GetFileByServerRelativeUrl(wspPath);
+            _context.Load(wsp);
+            _context.ExecuteQuery();
+
+            _context.Load(wsp.ListItemAllFields);
+            _context.ExecuteQuery();
+
+            var itemId = wsp.ListItemAllFields["ID"];
+
+            if (wsp.Exists)
+            {
+
+              Console.WriteLine("365: Deactivating solution...");
+
+              _deactivation = new System.Windows.Forms.Form { WindowState = FormWindowState.Minimized };
+              _deactivation.SuspendLayout();
+              var webBrowser = new WebBrowser();
+              webBrowser.Navigate(_site + "_catalogs/solutions/Forms/Activate.aspx?deploy=true&Op=DEA&ID=" + itemId);
+              webBrowser.Navigated += webBrowser_Navigated;
+              _deactivation.Controls.Add(webBrowser);
+              _deactivation.ResumeLayout(false);
+
+              Application.Run(_deactivation);
+
+              while (!_isDeactivated) // await deactivation
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine("365: Uploading solution...");
+
+            var solutionList = _context.Site.GetCatalog(121);
+
+            var info = new FileCreationInformation
+            {
+              Overwrite = true,
+              Url = System.IO.Path.GetFileName(_path),
+              Content = System.IO.File.ReadAllBytes(_path)
+            };
+
+            var solution = solutionList.RootFolder.Files.Add(info);
+            _context.Load(solution);
+            _context.ExecuteQuery();
+
+            Console.WriteLine("365: Activating solution...");
+
+            _activation = new System.Windows.Forms.Form { WindowState = FormWindowState.Minimized };
+            _activation.SuspendLayout();
+            var webBrowserAct = new WebBrowser();
+            webBrowserAct.Navigate(_site + "_catalogs/solutions/Forms/Activate.aspx?deploy=true&Op=ACT&ID=" + itemId);
+            webBrowserAct.Navigated += webBrowserAct_Navigated;
+            _activation.Controls.Add(webBrowserAct);
+            _activation.ResumeLayout(false);
+
+            Application.Run(_activation);
+
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.Write(ex.ToString());
+        if (args.Length == 3 && args[2] == "debug")
+          throw;
+      }
+    }
+
+    static void webBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+    {
+      if (!e.Url.AbsolutePath.Contains("AllItems.aspx"))
+        return;
+
+      _deactivation.Close();
+      _isDeactivated = true;
+    }
+
+    static void webBrowserAct_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+    {
+      if (!e.Url.AbsolutePath.Contains("AllItems.aspx"))
+        return;
+
+      _activation.Close();
+      Console.WriteLine("365: Solution deployed.");
+    }
+  }
+}
